b4939afa6f42b4787b9aa10abfb68e5fd4c0bd6d
eternal-silence00 <andron555666777@gmail.com>
2026-06-02 19:53:39 +0300

    feat: implement RAG pipeline with pgvector context retrieval

--- app/routers/message.py
+++ app/routers/message.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.schemes.message import MessageCreate, MessageResponse
 from app.services.ai_service import AIService
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.database import get_db
 
 router = APIRouter()
 
 @router.post("/message", response_model=MessageResponse)
-def create_message(
-    data: MessageCreate
+async def create_message(
+    data: MessageCreate,
+    session: AsyncSession = Depends(get_db),
+    limit: int = 3
 ):
     chat = AIService()
-    response = chat.get_message(data.message, data.session_id)
+    response = await chat.get_message(session, data.message, data.session_id, limit)
     return {"message": response}
--- app/services/ai_service.py
+++ app/services/ai_service.py
@@ -1,19 +1,27 @@
 from groq import Groq
 from app.services.redis_service import RedisService
+from app.services.document_service import DocumentService
 import os
+from sqlalchemy.ext.asyncio import AsyncSession
 
 class AIService:
     
     def __init__(self):
         self.client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+        self.DocService = DocumentService()
         
-    def get_message(self, message: str, session_id) -> str:
+    async def get_message(self, session: AsyncSession, message: str, session_id, limit: int) -> str:
         redis_client = RedisService()
         history = redis_client.get_history(session_id)
         history.append({'role': "user", "content": message})
+        similar = await self.DocService.find_simular(session, message, limit)
+        context = "\n".join([doc.content for doc in similar])
         response = self.client.chat.completions.create(
             model="llama-3.1-8b-instant",
-            messages=history
+            messages=[
+                {"role": "system", "content": f"Отвечай только на основе этих документов:\n{context}"},
+                *history
+            ]
         )
         redis_client.add_message(session_id, "user", message)
         answer = response.choices[0].message.content
